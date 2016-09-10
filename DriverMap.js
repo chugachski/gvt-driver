8b39be0fd6cd133fa0a06bbf89b2a16817e34629
Nathan Smith <natesmith.copper@gmail.com>
2016-09-10 17:55:12 -0400

    using geo

--- DriverMap.js
+++ DriverMap.js
@@ -15,35 +15,33 @@ class DriverMap extends Component {
     super(props);
     this.state = {
       shuttleId: '',
-      driverId: '',
 
       mapRegion: {
-        latitude: 60.965727,
-        longitude: -149.136103,
+        // latitude: 60.965727,
+        // longitude: -149.136103,
+        latitude: 37.774929,
+        longitude: -122.419416,
         latitudeDelta: 0.15,
         longitudeDelta: 0.15,
       },
       annotations: [{
-        latitude: 60.965727,
-        longitude: -149.136103,
-      }],
+        latitude: 0,
+        longitude: 0
+      }]
     };
   }
 
-  goBack(){
-    this.props.navigator.pop()
-  }
-
   setLoc(){
+    const app = this;
     navigator.geolocation.watchPosition(function(position) {
       axios({
         method: 'PUT',
-        url: `http://localhost:3000/trackings/${this.state.shuttleId}`,
+        url: `http://localhost:3000/trackings/${app.state.shuttleId}`,
         data: {
           new_lat: position.coords.latitude,
           new_lng: position.coords.longitude,
-          new_driver_id: this.props.driver.id,
-          new_shuttle_id: this.props.shuttle.id
+          new_driver_id: app.props.driver.id,
+          new_shuttle_id: app.props.shuttle.id
         }
       })
     });
@@ -54,7 +52,6 @@ class DriverMap extends Component {
   }
 
   logout(){
-    // const ape = this
     console.log(this.props.shuttle.id);
     axios({
       method: 'DELETE',
@@ -68,47 +65,50 @@ class DriverMap extends Component {
     // location instead of starting it at Tramway before the next update.
     // Leaving it hardcoded until testing on watchPosition is done.
 
-    // const options = {
-    //   enableHighAccuracy: true,
-    //   timeout: 15000,
-    //   maximumAge: 1000
-    // }
-    //
-    // navigator.geolocation.getCurrentPosition(
-    //   position => {
-    //     console.log('shuttlePos:', position);
-    //     // set state
-    //     this.setState({
-    //       annotations: [{
-    //         latitude: position.coords.latitude,
-    //         longitude: position.coords.longitude,
-    //       }]
-    //     })
-    //   },
-    //   (error) => alert(error.message), options
-    // );
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 15000,
+      maximumAge: 1000
+    }
 
+    const app = this;
 
-    const app = this
-      axios({
-        method: 'POST',
-        url: 'http://localhost:3000/trackings/new',
-        data: {
-          lat: 60.970598,
-          lng: -149.096939,
-          driver_id: this.props.driver.id,
-          shuttle_id: this.props.shuttle.id
-        }
-      }).then(function(response){
-        console.log(response);
-        // app.setState({shuttleId: response.data[0].id})
-        // app.setLoc()
-      })
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log('shuttlePos:', position);
+        this.setState({
+          annotations: [{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }]
+        })
+        // console.log('lat', app.state.annotations[0].latitude);
+
+        // post initial shuttle position to db
+        // this needs to happen AFTER geolocation is finished and state is set
+        axios({
+          method: 'POST',
+          url: 'http://localhost:3000/trackings/new',
+          data: {
+            // lat: 60.970598,
+            // lng: -149.096939,
+            lat: this.state.annotations[0].latitude,
+            lng: this.state.annotations[0].longitude,
+            driver_id: this.props.driver.id,
+            shuttle_id: this.props.shuttle.id
+          }
+        }).then(function(response){
+          console.log('R:', response);
+          app.setState({shuttleId: response.data.id})
+          // app.setLoc()
+        })
+      },
+      (error) => alert(error.message), options
+    );
   }
 
   render(){
-    // console.log('t.p.:', this.props);
-
+    console.log('t.p.:', this.props);
     function formatNames(first, last) {
       return(
         first.charAt(0).toUpperCase() +
@@ -120,7 +120,6 @@ class DriverMap extends Component {
 
     return(
       <View style={styles.container}>
-
         <MapView
           style={styles.map}
           region={this.state.mapRegion}
